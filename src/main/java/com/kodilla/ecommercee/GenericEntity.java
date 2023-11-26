930195f6b2cf308312cc31d7d6f96e55e26f8fd5
Adam Podlaski <podlaski83@gmail.com>
2023-11-26 22:45:51 +0100

    task JDP231201-1 unnecessary classes/interfaces removed, ProductController improved

--- src/main/java/com/kodilla/ecommercee/GenericEntity.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntity.java
@@ -1,11 +1,11 @@
 package com.kodilla.ecommercee;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
+@Data
 public class GenericEntity {
 
     @Id
--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,50 +1,41 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.model.repository.ProductRepository;
-import com.kodilla.ecommercee.service.ProductServiceImpl;
 import com.kodilla.ecommercee.service.dto.ProductDto;
-import com.kodilla.ecommercee.service.mapper.ProductMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/products")
-@RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductServiceImpl productService;
-    private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
-
     @GetMapping()
-    public List<ProductDto> getAllProducts() {
-        return productRepository.findAll().stream()
-                .map(productMapper::mapToProductDto)
-                .collect(Collectors.toList());
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>(List.of(
+                new ProductDto(1L, "Dummy Product One"),
+                new ProductDto(2L, "Dummy Product Two"),
+                new ProductDto(3L, "Dummy Product Three")
+        ));
     }
 
-    @GetMapping(value = "{productId}")
-    public ProductDto getProduct(@PathVariable Long productId) {
-        return new ProductDto(1L, "in this method getProduct returns always the same value no matter the @PathVariable param");
+    @GetMapping("/{productId}")
+    public ProductDto getProduct(@PathVariable("productId") Long productId) {
+        return new ProductDto(1L, "Dummy Product");
     }
 
-    @DeleteMapping(value = "{productId}")
-    public void deleteProduct(@PathVariable Long productId) {
-        productRepository.deleteById(productId);
+    @PostMapping("/create")
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        return new ProductDto(1L, "Newly Created Dummy Product");
     }
 
-    @PutMapping()
+    @PutMapping("/update")
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        productDto = new ProductDto(2L, "updated product no matter the json body");
-        productRepository.save(productMapper.mapToProduct(productDto));
-        return productDto;
+        return new ProductDto(1L, "Updated Dummy Product");
     }
 
-    @PostMapping
-    public ProductDto createProduct(ProductDto productDto) {
-        return new ProductDto();
+    @DeleteMapping("/delete/{productId}")
+    public void deleteProduct(@PathVariable("productId") Long productId) {
+
     }
 }
