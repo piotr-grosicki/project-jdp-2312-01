2c43700a5cf7d4916548ed977ad74314c18c5ca4
Izabela Bohdziewicz <izabelabohdziewicz13@wp.pl>
2023-11-27 15:48:35 +0100

    OrderController & OrderDto

---
+++ src/main/java/com/kodilla/ecommercee/service/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package com.kodilla.ecommercee.service.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class OrderDto {
+
+    private Long id;
+    private String productName;
+    private String productDescription;
+
+
+}
