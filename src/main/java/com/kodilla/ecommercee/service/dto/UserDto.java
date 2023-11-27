a6e79ea21537d070c464a48add04604d00015f10
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-11-27 16:36:52 +0100

    UserController

---
+++ src/main/java/com/kodilla/ecommercee/service/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String userName;
+
+}
