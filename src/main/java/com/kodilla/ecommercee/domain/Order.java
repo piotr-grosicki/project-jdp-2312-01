6c009d5315b2fb48f0ea9db222c69f2284acb252
Izabela Bohdziewicz <izabelabohdziewicz13@wp.pl>
2023-11-30 10:57:06 +0100

    OrderController & OrderDto

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,29 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.List;
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_name")
+    private String orderName;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
--- src/main/java/com/kodilla/ecommercee/service/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/service/dto/GroupDto.java
@@ -1,7 +1,9 @@
 package com.kodilla.ecommercee.service.dto;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class GroupDto {
 
     private Long id;
