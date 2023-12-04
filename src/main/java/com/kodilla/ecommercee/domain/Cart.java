0ca85bfa56278790c8d38ad3ebe8f3141c1a79e8
Adam Podlaski <podlaski83@gmail.com>
2023-12-04 23:17:24 +0100

    task JDP231201-15 Order, User, Cart Entities + Repositories created

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,25 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @OneToOne(mappedBy = "cart")
+    private User user;
+}
