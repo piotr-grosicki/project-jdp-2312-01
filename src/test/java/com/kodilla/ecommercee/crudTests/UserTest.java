2383062073b7b046495c2e62bc3e2f9f5cf68caf
Adam Podlaski <podlaski83@gmail.com>
2023-12-16 12:19:52 +0100

    CartTest and UserTest corrections

--- src/test/java/com/kodilla/ecommercee/crudTests/UserTest.java
+++ src/test/java/com/kodilla/ecommercee/crudTests/UserTest.java
@@ -86,10 +86,10 @@ public class UserTest {
         products.add(product1);
 
         User user = new User(null, "TestUser", null, null);
-        userRepository.save(user);
+        user = userRepository.save(user);
 
         Cart cart = new Cart(1L, user, products);
-        cartRepository.save(cart);
+        cart = cartRepository.save(cart);
 
         // When
         cartRepository.deleteById(1L);
