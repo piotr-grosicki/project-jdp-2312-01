4e1d2554f4b549574284ab19c5ed15b5e9d4e1ae
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-12-18 22:46:48 +0100

    User implementation

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.exceptions.UserNotFoundException;
 import com.kodilla.ecommercee.service.UserService;
 import com.kodilla.ecommercee.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
@@ -18,27 +19,46 @@ public class UserController {
 
     @GetMapping()
     public ResponseEntity<List<UserDto>> getAllUsers() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.FOUND);
+        try {
+            return new ResponseEntity<>(userService.getUserById(id)
+                    , HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping()
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
-        return new ResponseEntity<>(userService.saveUser(userDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long id, @RequestBody UserDto userDto) {
-        return new ResponseEntity<>(userService.updateUser(userDto), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(userService.updateUser(id, userDto), HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        try {
+            userService.deleteUser(id)
+            ;
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/com/kodilla/ecommercee/service/mapper/UserMapper.java
+++ src/main/java/com/kodilla/ecommercee/service/mapper/UserMapper.java
@@ -1,26 +1,58 @@
 package com.kodilla.ecommercee.service.mapper;
 
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.exceptions.CartNotFoundException;
+import com.kodilla.ecommercee.model.repository.CartRepository;
 import com.kodilla.ecommercee.service.dto.UserDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
 public class UserMapper {
 
-    public static UserDto mapToUserDto(User user) {
+    private final CartRepository cartRepository;
+
+    public UserDto mapToUserDto(User user) {
         return new UserDto(
                 user.getId(),
                 user.getUserName(),
-                user.getCart(),
-                user.getOrders()
+                user.getCart().getId(),
+                mapToIds(user.getOrders())
+
         );
     }
 
-    public static User mapToUser(UserDto userDto) {
-        return new User(
+    public User mapToUser(UserDto userDto) {
+        User user = new User(
                 userDto.getId(),
                 userDto.getUserName(),
-                userDto.getCart(),
-                userDto.getOrders()
+                cartRepository.findById(userDto.getCartId()).get(),
+                Collections.emptyList()
         );
+        Cart cart = new Cart();
+        cart.setId(userDto.getId());
+        user.setCart(cart);
+
+        return user;
+    }
+
+    private List<Long> mapToIds(List<?> entities) {
+        return entities.stream()
+                .map(entity -> {
+                    if (entity instanceof Order) {
+                        return ((Order) entity).getId();
+                    } else {
+                        return null;
+                    }
+                })
+                .collect(Collectors.toList());
     }
 
 }
